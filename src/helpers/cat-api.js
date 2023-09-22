5e096473a242a8188111b148e83c3d6170ac0fa1
Oksana Jarosch <ok.jarosch@gmail.com>
2023-09-23 00:52:54 +0200

    options for select

--- src/helpers/cat-api.js
+++ src/helpers/cat-api.js
@@ -1,6 +1,18 @@
 export const BASE_URL = "https://api.thecatapi.com/v1";
 const ENDPOINT = "images/search";
+const SEARCH_ENDPOINT = "breeds";
 
-// export function fetchBreeds(){
+export function fetchBreeds(){
+return fetch(`${BASE_URL}/${SEARCH_ENDPOINT}`)
+.then(response => {
+    if(!response.ok){
+        throw new Error (response.statusText)
+    }
+    return response.json()
+})
+}
+
+
+// fetchCatByBreed(breedId){
 
 // }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import axios from "axios";
+import { fetchBreeds } from "./helpers/cat-api";
 
 axios.defaults.headers.common["x-api-key"] = "live_Luv3WqWjgqS027nk6cq78VcSpdK30ZZfuGi6yp6MDvY55qKhq1CZWlAnFh8nf7vz";
 
-const SEARCH_ENDPOINT = "breeds";
+const selectPlaceholder = `<option class="js-selectOption" value="choose">Select the cat</option>`;
+let query = "";
+
 const select = document.querySelector(".breed-select");
+
+select.addEventListener("change", onChangeSelect);
+
+select.insertAdjacentHTML("afterbegin", selectPlaceholder);
+
+function markupSelect(arr) {
+   return arr.map(({name, id}) => {
+    return `<option class="js-selectOption" value="${id}">${name}</option>`
+   }).join("");
+}
+
+fetchBreeds()
+.then(data => {
+    select.insertAdjacentHTML("beforeend", markupSelect(data))
+})
+.catch(err => console.log(err))
+
+function onChangeSelect() {
+    query = select.value;
+    // console.log(query)
+}
