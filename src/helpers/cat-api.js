88d9d502844edd40801f7b07241b7329b095f630
Oksana Jarosch <ok.jarosch@gmail.com>
2023-09-24 00:03:45 +0200

    cat-search

--- src/helpers/cat-api.js
+++ src/helpers/cat-api.js
@@ -1,6 +1,11 @@
-export const BASE_URL = "https://api.thecatapi.com/v1";
+const BASE_URL = "https://api.thecatapi.com/v1";
 const ENDPOINT = "images/search";
 const SEARCH_ENDPOINT = "breeds";
+const options = {
+    headers: {
+        "x-api-key": "live_Luv3WqWjgqS027nk6cq78VcSpdK30ZZfuGi6yp6MDvY55qKhq1CZWlAnFh8nf7vz"
+    }
+}
 
 export function fetchBreeds(){
 return fetch(`${BASE_URL}/${SEARCH_ENDPOINT}`)
@@ -13,6 +18,13 @@ return fetch(`${BASE_URL}/${SEARCH_ENDPOINT}`)
 }
 
 
-// fetchCatByBreed(breedId){
+export function fetchCatByBreed(breedId){
+return fetch(`${BASE_URL}/${ENDPOINT}?breed_ids=${breedId}`, options)
+.then(response => {
+    if(!response.ok){
+        throw new Error (response.statusText)
+    }
+    return response.json()
+})
+}
 
-// }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import axios from "axios";
 import { fetchBreeds } from "./helpers/cat-api";
+import { fetchCatByBreed } from "./helpers/cat-api";
 
-axios.defaults.headers.common["x-api-key"] = "live_Luv3WqWjgqS027nk6cq78VcSpdK30ZZfuGi6yp6MDvY55qKhq1CZWlAnFh8nf7vz";
-
+// Globale constanten
 const selectPlaceholder = `<option class="js-selectOption" value="choose">Select the cat</option>`;
-let query = "";
 
+// Query selectors
 const select = document.querySelector(".breed-select");
-
-select.addEventListener("change", onChangeSelect);
+const catInfoCard = document.querySelector(".cat-info");
+const loaderMessage = document.querySelector(".loader");
+const errorMessage = document.querySelector(".error");
 
 select.insertAdjacentHTML("afterbegin", selectPlaceholder);
 
@@ -24,7 +24,42 @@ fetchBreeds()
 })
 .catch(err => console.log(err))
 
+// Event listener for select
+select.addEventListener("change", onChangeSelect);
+
 function onChangeSelect() {
-    query = select.value;
-    // console.log(query)
+    errorMessage.hidden = true;
+    loaderMessage.hidden = false;
+    select.hidden = true;
+    // console.log(select.value);
+   fetchCatByBreed(select.value)
+   .then(data => {
+    // console.log(data[0]);
+    const img = data[0].url;
+    const name = data[0].breeds[0].name;
+    const description = data[0].breeds[0].description;
+    const temperament = data[0].breeds[0].temperament;
+   
+catInfoCard.innerHTML = createCatCard(img, name, description, temperament);
+loaderMessage.hidden = true;
+select.hidden = false;
+})
+.catch(err => {
+    errorMessage.hidden = false;
+    loaderMessage.hidden = true;
+select.hidden = false;
+    console.log(err)
+    catInfoCard.innerHTML = "";
+})
+}
+
+    function createCatCard(img, name, description, temperament){
+
+    return `<img class="js-cat-photo" src="${img}" alt="${name}" width="300">
+    <div class="js-cat-description">
+      <h2>${name}</h2>
+      <p>${description}</p>
+      <h3>Temperament:</h3>
+      <p>${temperament}</p>
+    </div>`;
 }
